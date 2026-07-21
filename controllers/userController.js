4786766c286ff364f1c84a497aa629014f7bf333
Aditya Jadhav <a.jadhav.work@gmail.com>
2026-07-21 21:29:52 +0500

    add get all users API

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,6 +19,26 @@ const createUser = async (req,res) =>{
     }
 };
 
+
+
+const getAllUsers = async (req,res) => {
+    try{
+        const users = await User.find();
+
+        res.status(200).json({
+            message:"Users Fetched successfully",
+            users,
+        });
+
+    } catch(error){
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+
+};
+
 module.exports = {
     createUser,
+    getAllUsers
 };
